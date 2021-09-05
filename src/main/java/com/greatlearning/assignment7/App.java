e06d6381c7b7a9eaeedb792e23a50537b4aae66a
Paarth Bir <42001307+paarthbir77@users.noreply.github.com>
2021-09-05 22:18:13 +0530

    update app.java

--- src/main/java/com/greatlearning/assignment7/App.java
+++ src/main/java/com/greatlearning/assignment7/App.java
@@ -11,7 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         int choice;
         String continueChoice;
         String packageInput;
